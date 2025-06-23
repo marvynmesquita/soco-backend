c39d271798c39c4eb92d3914a59afa353276e785
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2025-06-23 09:38:32 -0300

    fix: corrige a escuta do servidor para permitir conexões externas

--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
 
 app.use('/api', busRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
